647dbe0a0a8ef56742194178d56aac933a9594b8
Nikhilv77 <nv581339@gmail.com>
2025-02-18 23:03:30 +0530

    binary search

---
+++ functions_and_arrays/BinarySearch.java
@@ -0,0 +1,36 @@
+package functions_and_arrays;
+
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int key = scn.nextInt();
+        System.out.println(binarySearch(arr, key));
+        scn.close();
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        Arrays.sort(arr);
+        int low = 0;
+        int high = arr.length - 1;
+        int answer = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] < key) {
+                low = mid + 1;
+            } else if (arr[mid] > key) {
+                high = mid - 1;
+            } else {
+                answer = mid;
+                break;
+            }
+        }
+        return answer;
+    }
+}
